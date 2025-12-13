b53a3ed44a31cbf4a8d94e5caa74dc025d7eabbd
Monsster <hefestoalan@gmail.com>
2025-12-13 20:23:32 -0300

    add

--- src/helpers/axiosConfig.js
+++ src/helpers/axiosConfig.js
@@ -1,18 +1,9 @@
 import axios from 'axios';
-import { getCookie } from './cookieHelper.js'; // ✅ importamos la función
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BACKEND_URL, 
-  withCredentials: true, // muy importante para cookies cross-site
+  withCredentials: true
 });
 
-// -------------------- INTERCEPTOR PARA SAFARI / iOS --------------------
-axiosInstance.interceptors.request.use((config) => {
-  const token = getCookie("token"); // lee la cookie 'token'
-  if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`; // fallback si Safari no envía cookie
-  }
-  return config;
-});
 
 export default axiosInstance;
